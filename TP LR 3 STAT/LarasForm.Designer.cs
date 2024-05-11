87453eb8aff54980a7abb6c37344a481bb313a45
RACER0617 <astanmorokhiya@gmail.com>
2024-05-11 14:40:35 +0300

    ФОРМА НАСЕЛЕНИЯ РФ

---
+++ TP LR 3 STAT/LarasForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace TP_LR_3_STAT
+{
+    partial class LarasForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.ChartPopulation = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.IblMaxGrowth = new System.Windows.Forms.Label();
+            this.IblMaxDecline = new System.Windows.Forms.Label();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.ButtonOpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonForecast = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PrognozChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartPopulation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PrognozChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChartPopulation
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.ChartPopulation.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.ChartPopulation.Legends.Add(legend3);
+            this.ChartPopulation.Location = new System.Drawing.Point(449, 12);
+            this.ChartPopulation.Name = "ChartPopulation";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.ChartPopulation.Series.Add(series3);
+            this.ChartPopulation.Size = new System.Drawing.Size(828, 392);
+            this.ChartPopulation.TabIndex = 0;
+            this.ChartPopulation.Text = "chart1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // IblMaxGrowth
+            // 
+            this.IblMaxGrowth.AutoSize = true;
+            this.IblMaxGrowth.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IblMaxGrowth.Location = new System.Drawing.Point(14, 53);
+            this.IblMaxGrowth.Name = "IblMaxGrowth";
+            this.IblMaxGrowth.Size = new System.Drawing.Size(0, 25);
+            this.IblMaxGrowth.TabIndex = 1;
+            // 
+            // IblMaxDecline
+            // 
+            this.IblMaxDecline.AutoSize = true;
+            this.IblMaxDecline.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IblMaxDecline.Location = new System.Drawing.Point(14, 124);
+            this.IblMaxDecline.Name = "IblMaxDecline";
+            this.IblMaxDecline.Size = new System.Drawing.Size(0, 25);
+            this.IblMaxDecline.TabIndex = 2;
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(113, 331);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.Size = new System.Drawing.Size(245, 358);
+            this.DataGridView.TabIndex = 3;
+            // 
+            // ButtonOpenFile
+            // 
+            this.ButtonOpenFile.Location = new System.Drawing.Point(39, 219);
+            this.ButtonOpenFile.Name = "ButtonOpenFile";
+            this.ButtonOpenFile.Size = new System.Drawing.Size(173, 67);
+            this.ButtonOpenFile.TabIndex = 4;
+            this.ButtonOpenFile.Text = "Загрузить файл";
+            this.ButtonOpenFile.UseVisualStyleBackColor = true;
+            this.ButtonOpenFile.Click += new System.EventHandler(this.ButtonOpenFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Максимальный прирост населения";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(306, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Максимальная убыль населения";
+            // 
+            // ButtonForecast
+            // 
+            this.ButtonForecast.Location = new System.Drawing.Point(235, 219);
+            this.ButtonForecast.Name = "ButtonForecast";
+            this.ButtonForecast.Size = new System.Drawing.Size(173, 67);
+            this.ButtonForecast.TabIndex = 7;
+            this.ButtonForecast.Text = "Прогноз на N лет по скользящей средней";
+            this.ButtonForecast.UseVisualStyleBackColor = true;
+            this.ButtonForecast.Click += new System.EventHandler(this.ButtonForecast_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.IblMaxDecline);
+            this.panel1.Controls.Add(this.IblMaxGrowth);
+            this.panel1.Location = new System.Drawing.Point(24, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(419, 166);
+            this.panel1.TabIndex = 8;
+            // 
+            // PrognozChart
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.PrognozChart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.PrognozChart.Legends.Add(legend4);
+            this.PrognozChart.Location = new System.Drawing.Point(449, 410);
+            this.PrognozChart.Name = "PrognozChart";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.PrognozChart.Series.Add(series4);
+            this.PrognozChart.Size = new System.Drawing.Size(828, 357);
+            this.PrognozChart.TabIndex = 9;
+            this.PrognozChart.Text = "chart1";
+            // 
+            // LarasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1322, 805);
+            this.Controls.Add(this.PrognozChart);
+            this.Controls.Add(this.ButtonForecast);
+            this.Controls.Add(this.ButtonOpenFile);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.ChartPopulation);
+            this.Controls.Add(this.panel1);
+            this.Name = "LarasForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ChartPopulation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PrognozChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartPopulation;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label IblMaxGrowth;
+        private System.Windows.Forms.Label IblMaxDecline;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Button ButtonOpenFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonForecast;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart PrognozChart;
+    }
+}
---
+++ TP LR 3 STAT/LarasForm.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using CsvHelper;
+using CsvHelper.Configuration;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.WinForms;
+using LiveCharts.Wpf;
+using Microsoft.Win32;
+
+namespace TP_LR_3_STAT
+{
+    public partial class LarasForm : Form
+    {
+        private List<PopulationData> populationDataList;
+
+        public LarasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Добавляем ось X с заголовком "Год"
+            ChartPopulation.ChartAreas[0].AxisX.Title = "Год";
+
+            // Добавляем ось Y с заголовком "Численность населения"
+            ChartPopulation.ChartAreas[0].AxisY.Title = "Численность населения";
+
+        }
+
+
+
+        private void ButtonOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    Delimiter = ";", // Указываем табуляцию как разделитель
+                    HeaderValidated = null // Игнорируем проверку заголовков
+                };
+
+                using (var reader = new StreamReader(openFileDialog1.FileName))
+                using (var csv = new CsvReader(reader, csvConfig))
+                {
+                    populationDataList = csv.GetRecords<PopulationData>().ToList();
+                }
+
+                DisplayDataInTable();
+                DrawPopulationChart();
+                CalculateGrowthAndDecline();
+            }
+        }
+
+        private void DisplayDataInTable()
+        {
+            DataGridView.DataSource = populationDataList;
+        }
+
+        private void DrawPopulationChart()
+        {
+            ChartPopulation.Series.Clear();
+            var series = ChartPopulation.Series.Add("Численность населения России");
+            series.ChartType = SeriesChartType.Line;
+
+            foreach (var data in populationDataList)
+            {
+                series.Points.AddXY(data.Year, data.Population);
+            }
+            ChartPopulation.ChartAreas[0].AxisY.Minimum = 13500000;
+            // Устанавливаем интервал на оси Y
+            ChartPopulation.ChartAreas[0].AxisY.Interval = 250000;
+        }
+
+
+        private void CalculateGrowthAndDecline()
+        {
+            double maxGrowth = 0;
+            double maxDecline = 0;
+            for (int i = 1; i < populationDataList.Count; i++)
+            {
+                double growthRate = (populationDataList[i].Population - populationDataList[i - 1].Population) / populationDataList[i - 1].Population * 100;
+                if (growthRate > maxGrowth)
+                {
+                    maxGrowth = growthRate;
+                }
+                else if (growthRate < maxDecline)
+                {
+                    maxDecline = growthRate;
+                }
+            }
+
+            IblMaxGrowth.Text = $"{maxGrowth}%";
+            IblMaxDecline.Text = $"{maxDecline}%";
+        }
+        private void ExtrapolateAndDrawChart(int yearsToExtrapolate)
+        {
+            // Создаем новую серию для прогноза
+            var extrapolatedSeries = new System.Windows.Forms.DataVisualization.Charting.Series("Прогноз на " + yearsToExtrapolate + " лет");
+            extrapolatedSeries.ChartType = SeriesChartType.Line;
+            extrapolatedSeries.Color = System.Drawing.Color.Red; // Устанавливаем цвет для линии прогнозирования
+
+            // Копируем существующие данные
+            var extrapolatedData = new List<PopulationData>(populationDataList);
+
+            // Производим экстраполяцию
+            for (int i = 0; i < yearsToExtrapolate; i++)
+            {
+                int lastYear = extrapolatedData.Last().Year;
+                double lastPopulation = extrapolatedData.Last().Population;
+                extrapolatedData.Add(new PopulationData
+                {
+                    Year = lastYear + 1,
+                    Population = lastPopulation * 1.02 // Например, просто увеличим на 2%
+                });
+            }
+
+            // Добавляем данные в серию для прогноза
+            foreach (var data in extrapolatedData)
+            {
+                extrapolatedSeries.Points.AddXY(data.Year, data.Population);
+            }
+
+            // Добавляем серию на график прогноза
+            PrognozChart.Series.Clear(); // Очищаем серии на графике прогноза, если они есть
+            PrognozChart.Series.Add(extrapolatedSeries);
+
+            // Устанавливаем оси и подписи для графика прогноза
+            PrognozChart.ChartAreas[0].AxisX.Title = "Год";
+            PrognozChart.ChartAreas[0].AxisY.Title = "Прогноз населения";
+            PrognozChart.ChartAreas[0].AxisY.Minimum = 13500000;
+            // Устанавливаем интервал на оси Y
+            PrognozChart.ChartAreas[0].AxisY.Interval = 250000;
+
+            // Перерисовываем график прогноза
+            PrognozChart.Invalidate();
+        }
+
+        private void ButtonForecast_Click(object sender, EventArgs e)
+        {
+
+            ExtrapolateAndDrawChart(5); // Экстраполируем данные на 5 лет и отображаем на графике
+        }
+    }
+    public class PopulationData
+    {
+        public int Year { get; set; }
+        public double Population { get; set; }
+    }
+}
